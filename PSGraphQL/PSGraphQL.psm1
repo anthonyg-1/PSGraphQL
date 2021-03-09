37898be3fd2517c04e73647269e224f353da13c0
Tony Guimelli <tony.guimelli@gmail.com>
2021-03-09 15:21:25 -0500

    Update PSGraphQL.psm1

--- PSGraphQL/PSGraphQL.psm1
+++ PSGraphQL/PSGraphQL.psm1
@@ -1,1 +1,14 @@
 
+<#
+.SYNOPSIS
+    This PowerShell module contains functions that facilitate querying and create, update, and delete (mutations) operations for GraphQL endpoints.
+.LINK
+    https://graphql.org/
+#>
+
+
+#region Load Public Functions
+
+Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 | Foreach-Object { . $_.FullName }
+
+#endregion
