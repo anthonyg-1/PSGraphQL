21f290adfc457393b577289476e63adc05ca215f
Tony Guimelli <tony.guimelli@gmail.com>
2021-03-10 11:42:57 -0500

    Update Invoke-GraphQLQuery.ps1
    Added parameter help and changed parameter order.

--- PSGraphQL/Functions/Invoke-GraphQLQuery.ps1
+++ PSGraphQL/Functions/Invoke-GraphQLQuery.ps1
@@ -4,6 +4,14 @@ function Invoke-GraphQLQuery {
         Sends a query to a GraphQL endpoint.
     .DESCRIPTION
         Sends a query (read operation) or mutation (create, update, delete operation) to a GraphQL endpoint.
+    .PARAMETER Query
+        The GraphQL query or mutation to send to the endpoint.
+    .PARAMETER Headers
+        Specifies the headers of the web request. Enter a hash table or dictionary.
+    .PARAMETER Uri
+        Specifies the Uniform Resource Identifier (URI) of the GraphQL endpoint to which the query or mutation is sent.
+    .PARAMETER Raw
+        For queries (not mutations) only. Tells the function to return JSON as opposed to objects.
     .NOTES
         Mutation results are returned as JSON strings while query results are implicitly returned as objects. To return a query result as JSON, use the -Raw switch.
     .EXAMPLE
@@ -22,7 +30,7 @@ function Invoke-GraphQLQuery {
 
         $requestHeaders = @{ myApiKey="aoMGY{+93dx&t!5)VMu4pI8U8T.ULO" }
 
-        Invoke-GraphQLQuery -Query $myQuery -Uri $url -Headers $requestHeaders -Raw
+        Invoke-GraphQLQuery -Query $myQuery -Headers $requestHeaders -Uri $url -Raw
 
         Sends a GraphQL query to the endpoint 'https://mytargetserver/v1/graphql' with the results returned as JSON.
     .EXAMPLE
@@ -41,7 +49,7 @@ function Invoke-GraphQLQuery {
 
         $requestHeaders = @{ myApiKey="aoMGY{+93dx&t!5)VMu4pI8U8T.ULO" }
 
-        $result = Invoke-GraphQLQuery -Query $myQuery -Uri $url -Headers $requestHeaders
+        $result = Invoke-GraphQLQuery -Query $myQuery -Headers $requestHeaders -Uri $url
         $result.data.users | Format-Table
 
         Sends a GraphQL query to the endpoint 'https://mytargetserver/v1/graphql' with the results returned as an object and navigates the hierarchy to return a table view of users.
@@ -58,7 +66,7 @@ function Invoke-GraphQLQuery {
 
         $requestHeaders = @{ myApiKey="aoMGY{+93dx&t!5)VMu4pI8U8T.ULO" }
 
-        $jsonResult = Invoke-GraphQLQuery -Mutation $myMutation -Uri $url -Headers $requestHeaders
+        $jsonResult = Invoke-GraphQLQuery -Mutation $myMutation -Headers $requestHeaders -Uri $url
 
         Sends a GraphQL mutation to the endpoint 'https://mytargetserver/v1/graphql' with the results returned as JSON.
     .LINK
@@ -74,13 +82,13 @@ function Invoke-GraphQLQuery {
             ValueFromPipelineByPropertyName = $false,
             Position = 0)][ValidateLength(12, 1073741791)][Alias("Mutation","q","m")][System.String]$Query,
 
-        [Parameter(Mandatory = $true,
-            ValueFromPipelineByPropertyName = $false,
-            Position = 1)][Alias("u")][System.Uri]$Uri,
-
         [Parameter(Mandatory = $false,
             ValueFromPipelineByPropertyName = $false,
-            Position = 2)][Alias("h")][System.Collections.Hashtable]$Headers,
+            Position = 1)][Alias("h")][System.Collections.Hashtable]$Headers,
+
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $false,
+            Position = 2)][Alias("u")][System.Uri]$Uri,
 
         [Parameter(Mandatory = $false,ParameterSetName="JSON",
             Position = 3)][Alias("AsJson","json","r")][Switch]$Raw
