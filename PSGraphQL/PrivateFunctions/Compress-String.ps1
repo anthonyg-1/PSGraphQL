433e698f7241436f603fd04bc4af2ea51778f50e
Benjamin Garside (v1) <114483383+benjamin-v1@users.noreply.github.com>
2025-04-07 12:23:40 +0100

    Update Compress-String.ps1

--- PSGraphQL/PrivateFunctions/Compress-String.ps1
+++ PSGraphQL/PrivateFunctions/Compress-String.ps1
@@ -1,5 +1,5 @@
 # Compresses and trims GraphQL operations for processing by this module's functions:
 function Compress-String([string]$InputString) {
-    $output = ([String]::Join(" ",($InputString.Split("`n")))).Trim()
+    $output = ($InputString -replace "`r`n|`n", " ").Trim()
     return $output
 }
